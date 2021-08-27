0c3413fb1395477ce443eb6a4215ca8d267d5ba2
alexward5 <alexanderward5@gmail.com>
2021-08-26 18:49:06 -0600

    install apollo client module + create Body component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
 
 function App() {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Body() {
+  return <div>IM A BODY</div>;
+}
+
+export default Body;
