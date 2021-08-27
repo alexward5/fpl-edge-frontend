535b3381b407827db33b0d473462ae7d5416d421
alexward5 <alexanderward5@gmail.com>
2021-08-26 19:32:43 -0600

    implement test query using apollo client

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
 function Body() {
+  const { loading, data } = useQuery(
+    gql`
+      query GetPlayerList {
+        players {
+          id
+          first_name
+          second_name
+        }
+      }
+    `,
+    { variables: { ids: [] } }
+  );
+
+  console.log(`LOADING: ${loading}`);
+  console.log("DATA", data);
   return <div>IM A BODY</div>;
 }
 
