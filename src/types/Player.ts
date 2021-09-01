70cf760d60f8d4fe036b657493670a2137bd7a44
alexward5 <alexanderward5@gmail.com>
2021-09-01 16:52:50 -0600

    update Player type to have nullable fields and an array of gamweek data

--- src/types/Player.ts
+++ src/types/Player.ts
@@ -5,8 +5,8 @@ interface Player {
   id: number;
   first_name: string;
   second_name: string;
-  player_season_totals: PlayerSeasonTotals;
-  player_gameweek_data: PlayerGameweekData;
+  player_season_totals?: PlayerSeasonTotals;
+  player_gameweek_data?: PlayerGameweekData[];
 }
 
 export default Player;
