dd18b83c5440c6f3c00a2dbdaf711ce492e3bbd6
Alex Ward <alexanderward5@gmail.com>
2025-02-07 15:42:26 -0700

    disable browser dev tools to silence alert

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import App from "./App.tsx";
 const client = new ApolloClient({
     uri: "http://localhost:4000/",
     cache: new InMemoryCache(),
+    connectToDevTools: false,
 });
 
 createRoot(document.getElementById("root")!).render(
