1928c122bf1b86675720490ad64fe4c446efc600
Alex Ward <alexanderward5@gmail.com>
2025-05-25 13:13:18 -0600

    remove unused import

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -9,7 +9,6 @@ import FormControl from "@mui/material/FormControl";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import { Typography } from "@mui/material";
 
 const drawerWidth = 240;
 
