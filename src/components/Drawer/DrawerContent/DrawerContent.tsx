6a1a781007c791aa9068977ce7e28f945fb458a7
Alex Ward <alexanderward5@gmail.com>
2025-07-16 17:23:22 -0600

    add border to part of drawer that isn't used in page header

--- src/components/Drawer/DrawerContent/DrawerContent.tsx
+++ src/components/Drawer/DrawerContent/DrawerContent.tsx
@@ -103,168 +103,178 @@ export default function DrawerContent(props: Props) {
                     sx={{ height: "42px", width: "auto" }}
                 />
             </Toolbar>
-            <Divider />
-            <Typography
-                variant="subtitle1"
-                fontWeight={"bold"}
-                sx={{ padding: "5px 0px 0px 18px" }}
-            >
-                Gameweeks
-            </Typography>
             <Box
                 sx={{
-                    width: "100%",
-                    height: "68px",
-                    padding: "0px 25px 0px 25px",
+                    display: "flex",
+                    flexDirection: "column",
+                    overflowY: "auto",
+                    borderTop: "1px solid rgba(0, 0, 0, 0.12)",
+                    borderRight: "1px solid rgba(0, 0, 0, 0.12)",
                 }}
             >
-                <GameweekSlider
-                    gameweekRange={gameweekRange}
-                    setGameweekRange={setGameweekRange}
-                />
-            </Box>
-            <Divider />
-            <Box
-                sx={{
-                    width: "100%",
-                    padding: "15px 15px 10px 15px",
-                }}
-            >
-                <CheckboxSelect
-                    listItems={["DEF", "MID", "FWD"]}
-                    label="Position"
-                    selectedList={displayedPositions}
-                    setSelectedList={setDisplayedPositions}
-                />
+                <Typography
+                    variant="subtitle1"
+                    fontWeight={"bold"}
+                    sx={{ padding: "5px 0px 0px 18px" }}
+                >
+                    Gameweeks
+                </Typography>
                 <Box
                     sx={{
                         width: "100%",
-                        marginTop: "15px",
-                        display: "flex",
-                        justifyContent: "space-between",
+                        height: "68px",
+                        padding: "0px 25px 0px 25px",
                     }}
                 >
-                    <TextField
-                        label="Min Price"
-                        type="number"
-                        value={playerPriceRange[0]}
-                        onChange={handleChangeMinPrice}
-                        inputMode="numeric"
-                        slotProps={{
-                            htmlInput: {
-                                step: 0.1,
-                                min: 0.0,
-                            },
-                        }}
-                        sx={{
-                            width: "48%",
-                        }}
-                    />
-                    <TextField
-                        label="Max Price"
-                        type="number"
-                        value={playerPriceRange[1]}
-                        onChange={handleChangeMaxPrice}
-                        inputMode="numeric"
-                        slotProps={{
-                            htmlInput: {
-                                step: 0.1,
-                                min: 0.0,
-                            },
-                        }}
-                        sx={{
-                            width: "48%",
-                        }}
+                    <GameweekSlider
+                        gameweekRange={gameweekRange}
+                        setGameweekRange={setGameweekRange}
                     />
                 </Box>
-            </Box>
-            <Divider />
-            <Box
-                sx={{
-                    width: "100%",
-                    padding: "10px 0px 0px 15px",
-                    flexGrow: 1,
-                    overflowY: "auto",
-                }}
-            >
-                <FormControl component="fieldset" variant="standard">
-                    <FormGroup>
-                        <FormControlLabel
-                            label={
-                                <Typography
-                                    variant="subtitle1"
-                                    fontWeight={"bold"}
-                                >
-                                    Teams
-                                </Typography>
-                            }
-                            control={
-                                <Checkbox
-                                    checked={
-                                        teamNames.length ===
-                                        displayedTeams.length
-                                    }
-                                    indeterminate={
-                                        teamNames.length !==
-                                        displayedTeams.length
-                                    }
-                                    onChange={() => {
-                                        if (
-                                            teamNames.length ===
-                                            displayedTeams.length
-                                        ) {
-                                            setDisplayedTeams([]);
-                                        } else {
-                                            setDisplayedTeams(teamNames);
-                                        }
-                                    }}
-                                    sx={{
-                                        height: "28px",
-                                    }}
-                                />
-                            }
+                <Divider />
+                <Box
+                    sx={{
+                        width: "100%",
+                        padding: "15px 15px 10px 15px",
+                    }}
+                >
+                    <CheckboxSelect
+                        listItems={["DEF", "MID", "FWD"]}
+                        label="Position"
+                        selectedList={displayedPositions}
+                        setSelectedList={setDisplayedPositions}
+                    />
+                    <Box
+                        sx={{
+                            width: "100%",
+                            marginTop: "15px",
+                            display: "flex",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <TextField
+                            label="Min Price"
+                            type="number"
+                            value={playerPriceRange[0]}
+                            onChange={handleChangeMinPrice}
+                            inputMode="numeric"
+                            slotProps={{
+                                htmlInput: {
+                                    step: 0.1,
+                                    min: 0.0,
+                                },
+                            }}
+                            sx={{
+                                width: "48%",
+                            }}
                         />
-                        {teamNames.map((teamName: string) => (
+                        <TextField
+                            label="Max Price"
+                            type="number"
+                            value={playerPriceRange[1]}
+                            onChange={handleChangeMaxPrice}
+                            inputMode="numeric"
+                            slotProps={{
+                                htmlInput: {
+                                    step: 0.1,
+                                    min: 0.0,
+                                },
+                            }}
+                            sx={{
+                                width: "48%",
+                            }}
+                        />
+                    </Box>
+                </Box>
+                <Divider />
+                <Box
+                    sx={{
+                        width: "100%",
+                        padding: "10px 0px 0px 15px",
+                        flexGrow: 1,
+                        overflowY: "auto",
+                    }}
+                >
+                    <FormControl component="fieldset" variant="standard">
+                        <FormGroup>
                             <FormControlLabel
-                                key={teamName}
+                                label={
+                                    <Typography
+                                        variant="subtitle1"
+                                        fontWeight={"bold"}
+                                    >
+                                        Teams
+                                    </Typography>
+                                }
                                 control={
                                     <Checkbox
-                                        checked={displayedTeams.includes(
-                                            teamName,
-                                        )}
-                                        // Toggle team in displayedTeams
-                                        onChange={(e) => {
-                                            if (e.target.checked) {
-                                                setDisplayedTeams([
-                                                    ...displayedTeams,
-                                                    teamName,
-                                                ]);
+                                        checked={
+                                            teamNames.length ===
+                                            displayedTeams.length
+                                        }
+                                        indeterminate={
+                                            teamNames.length !==
+                                            displayedTeams.length
+                                        }
+                                        onChange={() => {
+                                            if (
+                                                teamNames.length ===
+                                                displayedTeams.length
+                                            ) {
+                                                setDisplayedTeams([]);
                                             } else {
-                                                setDisplayedTeams(
-                                                    displayedTeams.filter(
-                                                        (team: string) =>
-                                                            team !== teamName,
-                                                    ),
-                                                );
+                                                setDisplayedTeams(teamNames);
                                             }
                                         }}
-                                        name={teamName}
-                                        size="small"
                                         sx={{
                                             height: "28px",
                                         }}
                                     />
                                 }
-                                sx={{ paddingLeft: "15px" }}
-                                label={
-                                    <Typography variant="subtitle2">
-                                        {teamName}
-                                    </Typography>
-                                }
                             />
-                        ))}
-                    </FormGroup>
-                </FormControl>
+                            {teamNames.map((teamName: string) => (
+                                <FormControlLabel
+                                    key={teamName}
+                                    control={
+                                        <Checkbox
+                                            checked={displayedTeams.includes(
+                                                teamName,
+                                            )}
+                                            // Toggle team in displayedTeams
+                                            onChange={(e) => {
+                                                if (e.target.checked) {
+                                                    setDisplayedTeams([
+                                                        ...displayedTeams,
+                                                        teamName,
+                                                    ]);
+                                                } else {
+                                                    setDisplayedTeams(
+                                                        displayedTeams.filter(
+                                                            (team: string) =>
+                                                                team !==
+                                                                teamName,
+                                                        ),
+                                                    );
+                                                }
+                                            }}
+                                            name={teamName}
+                                            size="small"
+                                            sx={{
+                                                height: "28px",
+                                            }}
+                                        />
+                                    }
+                                    sx={{ paddingLeft: "15px" }}
+                                    label={
+                                        <Typography variant="subtitle2">
+                                            {teamName}
+                                        </Typography>
+                                    }
+                                />
+                            ))}
+                        </FormGroup>
+                    </FormControl>
+                </Box>
             </Box>
         </div>
     );
