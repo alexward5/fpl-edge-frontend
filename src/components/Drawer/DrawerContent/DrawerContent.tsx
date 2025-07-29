d30b3ba3cb63c09629ff2f1d25b377eb444ea85d
Alex Ward <alexanderward5@gmail.com>
2025-07-29 17:20:08 -0600

    create separate TeamFilter component

--- src/components/Drawer/DrawerContent/DrawerContent.tsx
+++ src/components/Drawer/DrawerContent/DrawerContent.tsx
@@ -3,16 +3,12 @@ import { useQuery } from "@apollo/client";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Checkbox from "@mui/material/Checkbox";
-import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
 import GameweekSlider from "./GameweekSlider/GameweekSlider";
 import { gql } from "../../../__generated__/gql";
 import PlayerFilters from "./PlayerFilters/PlayerFilters";
+import TeamFilter from "./TeamFilter/TeamFilter";
 
-interface Props {
+type Props = {
     gameweekRange: number[];
     setGameweekRange: React.Dispatch<React.SetStateAction<number[]>>;
     displayedTeams: string[];
@@ -21,7 +17,7 @@ interface Props {
     setDisplayedPositions: React.Dispatch<React.SetStateAction<string[]>>;
     playerPriceRange: string[];
     setPlayerPriceRange: React.Dispatch<React.SetStateAction<string[]>>;
-}
+};
 
 const GET_TEAM_DATA = gql(`
     query GetTeamNames {
@@ -126,94 +122,11 @@ export default function DrawerContent(props: Props) {
                     handleChangeMaxPrice={handleChangeMaxPrice}
                 />
                 <Divider />
-                <Box
-                    sx={{
-                        width: "100%",
-                        padding: "10px 0px 0px 15px",
-                        flexGrow: 1,
-                        overflowY: "auto",
-                    }}
-                >
-                    <FormControl component="fieldset" variant="standard">
-                        <FormGroup>
-                            <FormControlLabel
-                                label={
-                                    <Typography
-                                        variant="subtitle1"
-                                        fontWeight={"bold"}
-                                    >
-                                        Teams
-                                    </Typography>
-                                }
-                                control={
-                                    <Checkbox
-                                        checked={
-                                            teamNames.length ===
-                                            displayedTeams.length
-                                        }
-                                        indeterminate={
-                                            teamNames.length !==
-                                            displayedTeams.length
-                                        }
-                                        onChange={() => {
-                                            if (
-                                                teamNames.length ===
-                                                displayedTeams.length
-                                            ) {
-                                                setDisplayedTeams([]);
-                                            } else {
-                                                setDisplayedTeams(teamNames);
-                                            }
-                                        }}
-                                        sx={{
-                                            height: "28px",
-                                        }}
-                                    />
-                                }
-                            />
-                            {teamNames.map((teamName: string) => (
-                                <FormControlLabel
-                                    key={teamName}
-                                    control={
-                                        <Checkbox
-                                            checked={displayedTeams.includes(
-                                                teamName,
-                                            )}
-                                            // Toggle team in displayedTeams
-                                            onChange={(e) => {
-                                                if (e.target.checked) {
-                                                    setDisplayedTeams([
-                                                        ...displayedTeams,
-                                                        teamName,
-                                                    ]);
-                                                } else {
-                                                    setDisplayedTeams(
-                                                        displayedTeams.filter(
-                                                            (team: string) =>
-                                                                team !==
-                                                                teamName,
-                                                        ),
-                                                    );
-                                                }
-                                            }}
-                                            name={teamName}
-                                            size="small"
-                                            sx={{
-                                                height: "28px",
-                                            }}
-                                        />
-                                    }
-                                    sx={{ paddingLeft: "15px" }}
-                                    label={
-                                        <Typography variant="subtitle2">
-                                            {teamName}
-                                        </Typography>
-                                    }
-                                />
-                            ))}
-                        </FormGroup>
-                    </FormControl>
-                </Box>
+                <TeamFilter
+                    teamNames={teamNames}
+                    displayedTeams={displayedTeams}
+                    setDisplayedTeams={setDisplayedTeams}
+                />
             </Box>
         </Box>
     );
---
+++ src/components/Drawer/DrawerContent/TeamFilter/TeamFilter.tsx
@@ -0,0 +1,98 @@
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Typography from "@mui/material/Typography";
+
+type Props = {
+    teamNames: string[];
+    displayedTeams: string[];
+    setDisplayedTeams: React.Dispatch<React.SetStateAction<string[]>>;
+};
+
+const TeamFilter: React.FC<Props> = ({
+    teamNames,
+    displayedTeams,
+    setDisplayedTeams,
+}) => (
+    <Box
+        sx={{
+            width: "100%",
+            padding: "10px 0px 0px 15px",
+            flexGrow: 1,
+            overflowY: "auto",
+        }}
+    >
+        <FormControl component="fieldset" variant="standard">
+            <FormGroup>
+                <FormControlLabel
+                    label={
+                        <Typography variant="subtitle1" fontWeight={"bold"}>
+                            Teams
+                        </Typography>
+                    }
+                    control={
+                        <Checkbox
+                            checked={teamNames.length === displayedTeams.length}
+                            indeterminate={
+                                teamNames.length !== displayedTeams.length
+                            }
+                            onChange={() => {
+                                if (
+                                    teamNames.length === displayedTeams.length
+                                ) {
+                                    setDisplayedTeams([]);
+                                } else {
+                                    setDisplayedTeams(teamNames);
+                                }
+                            }}
+                            sx={{
+                                height: "28px",
+                            }}
+                        />
+                    }
+                />
+                {teamNames.map((teamName: string) => (
+                    <FormControlLabel
+                        key={teamName}
+                        control={
+                            <Checkbox
+                                checked={displayedTeams.includes(teamName)}
+                                // Toggle team in displayedTeams
+                                onChange={(e) => {
+                                    if (e.target.checked) {
+                                        setDisplayedTeams([
+                                            ...displayedTeams,
+                                            teamName,
+                                        ]);
+                                    } else {
+                                        setDisplayedTeams(
+                                            displayedTeams.filter(
+                                                (team: string) =>
+                                                    team !== teamName,
+                                            ),
+                                        );
+                                    }
+                                }}
+                                name={teamName}
+                                size="small"
+                                sx={{
+                                    height: "28px",
+                                }}
+                            />
+                        }
+                        sx={{ paddingLeft: "15px" }}
+                        label={
+                            <Typography variant="subtitle2">
+                                {teamName}
+                            </Typography>
+                        }
+                    />
+                ))}
+            </FormGroup>
+        </FormControl>
+    </Box>
+);
+
+export default TeamFilter;
