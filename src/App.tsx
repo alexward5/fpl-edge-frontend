d06db1b772b13fd514ced6f5a46c8813aadb049f
Alex Ward <alexanderward5@gmail.com>
2025-08-01 16:07:49 -0600

    remove loading context + current loading indicator implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import "./App.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./mui-theme";
 import PageContent from "./components/PageContent/PageContent.tsx";
-import { LoadingProvider } from "./contexts/LoadingContext.tsx";
 
 function App() {
     return (
         <div className="app-contianer">
             <ThemeProvider theme={theme}>
-                <LoadingProvider>
-                    <PageContent />
-                </LoadingProvider>
+                <PageContent />
             </ThemeProvider>
         </div>
     );
--- src/components/PageContent/PageContent.tsx
+++ src/components/PageContent/PageContent.tsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
-import Box from "@mui/material/Box";
-import CircularProgress from "@mui/material/CircularProgress";
 import Header from "./Header/Header";
 import PlayerData from "../PlayerData/PlayerData";
-import { useTheme } from "@mui/material/styles";
-import { useLoading } from "../../contexts/LoadingContext";
 
 function PageContent() {
     const [mobileOpen, setMobileOpen] = useState(false);
@@ -25,42 +21,14 @@ function PageContent() {
         }
     };
 
-    const theme = useTheme();
-    const { isLoading } = useLoading();
-
     return (
         <>
             <Header handleDrawerToggle={handleDrawerToggle} />
-            {isLoading ? (
-                <Box
-                    sx={{
-                        height: {
-                            xs: `calc(100% - ${theme.appBarHeightXs})`,
-                            sm: `calc(100% - ${theme.appBarHeightSm})`,
-                        },
-                        mt: {
-                            xs: theme.appBarHeightXs,
-                            sm: theme.appBarHeightSm,
-                        },
-                        width: "100%",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <CircularProgress
-                        sx={{
-                            color: "black",
-                        }}
-                    />
-                </Box>
-            ) : (
-                <PlayerData
-                    handleDrawerClose={handleDrawerClose}
-                    handleDrawerTransitionEnd={handleDrawerTransitionEnd}
-                    mobileOpen={mobileOpen}
-                />
-            )}
+            <PlayerData
+                handleDrawerClose={handleDrawerClose}
+                handleDrawerTransitionEnd={handleDrawerTransitionEnd}
+                mobileOpen={mobileOpen}
+            />
         </>
     );
 }
