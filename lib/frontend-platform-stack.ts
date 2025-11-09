9df8b4d6d8f3a82f9ead89e21756bfe534ffb083
Alex Ward <alexanderward5@gmail.com>
2025-11-09 16:01:15 -0700

    add route53 + cert manager configs to stack

--- lib/frontend-platform-stack.ts
+++ lib/frontend-platform-stack.ts
@@ -4,75 +4,95 @@ import {
     aws_s3 as s3,
     aws_cloudfront as cloudfront,
     aws_cloudfront_origins as origins,
+    aws_certificatemanager as acm,
+    aws_route53 as route53,
+    aws_route53_targets as route53Targets,
 } from "aws-cdk-lib";
 
 export class FrontendPlatformStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        const apexDomain = "versostat.com";
+        const zone = route53.HostedZone.fromLookup(this, "Zone", {
+            domainName: apexDomain,
+        });
+
+        // Private S3 bucket for site content (CloudFront-only via OAC)
         const siteBucket = new s3.Bucket(this, "SiteBucket", {
             blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
             encryption: s3.BucketEncryption.S3_MANAGED,
             enforceSSL: true,
-            versioned: false,
         });
 
-        const headers = new cloudfront.ResponseHeadersPolicy(
+        // ACM Certificate for CloudFront
+        const cert = new acm.Certificate(this, "FrontendCert", {
+            domainName: apexDomain, // "versostat.com"
+            subjectAlternativeNames: [`www.${apexDomain}`], // "www.versostat.com"
+            validation: acm.CertificateValidation.fromDns(zone),
+        });
+        // CloudFront distribution with OAC
+        const distribution = new cloudfront.Distribution(
             this,
-            "SecurityHeaders",
+            "SiteDistribution",
             {
-                securityHeadersBehavior: {
-                    contentTypeOptions: { override: true },
-                    frameOptions: {
-                        frameOption: cloudfront.HeadersFrameOption.DENY,
-                        override: true,
-                    },
-                    referrerPolicy: {
-                        referrerPolicy:
-                            cloudfront.HeadersReferrerPolicy.NO_REFERRER,
-                        override: true,
-                    },
-                    strictTransportSecurity: {
-                        accessControlMaxAge: cdk.Duration.days(365),
-                        includeSubdomains: true,
-                        preload: true,
-                        override: true,
-                    },
-                    xssProtection: {
-                        protection: true,
-                        modeBlock: true,
-                        override: true,
-                    },
+                defaultRootObject: "index.html",
+                domainNames: [apexDomain, `www.${apexDomain}`], // ["versostat.com", "www.versostat.com"]
+                certificate: cert,
+                defaultBehavior: {
+                    origin: origins.S3BucketOrigin.withOriginAccessControl(
+                        siteBucket,
+                    ),
+                    viewerProtocolPolicy:
+                        cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                    compress: true,
+                    cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
                 },
+                errorResponses: [
+                    {
+                        httpStatus: 403,
+                        responseHttpStatus: 200,
+                        responsePagePath: "/index.html",
+                        ttl: cdk.Duration.seconds(0),
+                    },
+                    {
+                        httpStatus: 404,
+                        responseHttpStatus: 200,
+                        responsePagePath: "/index.html",
+                        ttl: cdk.Duration.seconds(0),
+                    },
+                ],
             },
         );
 
-        const dist = new cloudfront.Distribution(this, "SiteDistribution", {
-            defaultRootObject: "index.html",
-            defaultBehavior: {
-                origin: origins.S3BucketOrigin.withOriginAccessControl(
-                    siteBucket,
-                ),
-                viewerProtocolPolicy:
-                    cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-                compress: true,
-                cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
-                responseHeadersPolicy: headers,
-            },
-            errorResponses: [
-                {
-                    httpStatus: 403,
-                    responseHttpStatus: 200,
-                    responsePagePath: "/index.html",
-                    ttl: cdk.Duration.seconds(0),
-                },
-                {
-                    httpStatus: 404,
-                    responseHttpStatus: 200,
-                    responsePagePath: "/index.html",
-                    ttl: cdk.Duration.seconds(0),
-                },
-            ],
+        // Route 53 aliases: apex and www -> CloudFront
+        new route53.ARecord(this, "ApexA", {
+            zone,
+            recordName: "",
+            target: route53.RecordTarget.fromAlias(
+                new route53Targets.CloudFrontTarget(distribution),
+            ),
+        });
+        new route53.AaaaRecord(this, "ApexAAAA", {
+            zone,
+            recordName: "",
+            target: route53.RecordTarget.fromAlias(
+                new route53Targets.CloudFrontTarget(distribution),
+            ),
+        });
+        new route53.ARecord(this, "WwwA", {
+            zone,
+            recordName: "www",
+            target: route53.RecordTarget.fromAlias(
+                new route53Targets.CloudFrontTarget(distribution),
+            ),
+        });
+        new route53.AaaaRecord(this, "WwwAAAA", {
+            zone,
+            recordName: "www",
+            target: route53.RecordTarget.fromAlias(
+                new route53Targets.CloudFrontTarget(distribution),
+            ),
         });
 
         new cdk.CfnOutput(this, "FrontendBucketName", {
@@ -80,11 +100,11 @@ export class FrontendPlatformStack extends cdk.Stack {
             exportName: "VersoStat-FrontendBucketName",
         });
         new cdk.CfnOutput(this, "FrontendDistributionId", {
-            value: dist.distributionId,
+            value: distribution.distributionId,
             exportName: "VersoStat-FrontendDistributionId",
         });
         new cdk.CfnOutput(this, "FrontendDomainName", {
-            value: dist.domainName,
+            value: apexDomain,
             exportName: "VersoStat-FrontendDomainName",
         });
     }
