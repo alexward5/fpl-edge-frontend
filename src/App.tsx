60da15a4032a24d504a137562cbae2cf6ecd564f
Alex Ward <alexanderward5@gmail.com>
2025-11-24 20:17:07 -0700

    have app container to use relative positioning on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import "./App.css";
 import { ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import theme from "./mui-theme";
 import PageContent from "./components/PageContent/PageContent.tsx";
 
 function App() {
     return (
-        <div
-            className="app-contianer"
-            style={{ backgroundColor: theme.darkThemeSurfaceColor_1 }}
-        >
-            <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+            <Box
+                className="app-contianer"
+                sx={{
+                    backgroundColor: theme.darkThemeSurfaceColor_1,
+                    [theme.breakpoints.down("sm")]: {
+                        position: "relative",
+                        minHeight: "100vh",
+                    },
+                }}
+            >
                 <PageContent />
-            </ThemeProvider>
-        </div>
+            </Box>
+        </ThemeProvider>
     );
 }
 
