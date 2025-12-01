9fedc8811fddde13603ae1bd84680922ed1a3451
Alex Ward <alexanderward5@gmail.com>
2025-12-01 16:30:02 -0700

    remove divider component + fix breakpoint

--- src/components/PlayerDataDrawer/DrawerContent/DrawerContent.tsx
+++ src/components/PlayerDataDrawer/DrawerContent/DrawerContent.tsx
@@ -1,7 +1,6 @@
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import GameweekSlider from "./GameweekSlider/GameweekSlider";
-import Divider from "@mui/material/Divider";
 import PlayerFilters from "./PlayerFilters/PlayerFilters";
 import TeamFilter from "./TeamFilter/TeamFilter";
 import { useTheme } from "@mui/material/styles";
@@ -48,7 +47,7 @@ export default function DrawerContent(props: Props) {
                     borderBottom: `1px solid ${theme.darkThemeBorderColor}`,
                     borderRight: {
                         xs: `1px solid ${theme.darkThemeBorderColor}`,
-                        sm: "none",
+                        md: "none",
                     },
                 }}
             >
@@ -73,14 +72,12 @@ export default function DrawerContent(props: Props) {
                     gameweekRange={gameweekRange}
                     setGameweekRange={setGameweekRange}
                 />
-                <Divider sx={{ marginBottom: "8px" }} />
                 <PlayerFilters
                     displayedPositions={displayedPositions}
                     setDisplayedPositions={setDisplayedPositions}
                     playerPriceRange={playerPriceRange}
                     setPlayerPriceRange={setPlayerPriceRange}
                 />
-                <Divider sx={{ marginTop: "15px", marginBottom: "10px" }} />
                 <TeamFilter
                     displayedTeams={displayedTeams}
                     setDisplayedTeams={setDisplayedTeams}
