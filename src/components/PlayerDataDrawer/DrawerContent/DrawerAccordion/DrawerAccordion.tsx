bb3334a1d25527ca2e9553339608e535b3f2a6d4
Alex Ward <alexanderward5@gmail.com>
2025-12-02 17:54:17 -0700

    styling for drawer accordion component

--- src/components/PlayerDataDrawer/DrawerContent/DrawerAccordion/DrawerAccordion.tsx
+++ src/components/PlayerDataDrawer/DrawerContent/DrawerAccordion/DrawerAccordion.tsx
@@ -22,7 +22,7 @@ export default function DrawerAccordion(props: Props) {
             square
             sx={{
                 backgroundColor: theme.darkThemeSurfaceColor_1,
-                borderBottom: `1px solid ${theme.darkThemeBorderColor}`,
+                borderRadius: "4px",
                 "&:before": {
                     display: "none",
                 },
@@ -31,31 +31,36 @@ export default function DrawerAccordion(props: Props) {
             <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
                 sx={{
-                    margin: 0,
-                    padding: 0,
+                    height: "46px",
                     minHeight: "unset",
-                    "& .MuiAccordionSummary-content": {
-                        margin: 0,
-                        "&.Mui-expanded": {
-                            margin: 0,
-                        },
+                    margin: 0,
+                    padding: theme.spacing(0, 1),
+                    backgroundColor: theme.darkThemeSurfaceColor_2,
+                    borderRadius: "4px",
+                    border: "1px solid transparent",
+                    "&:hover": {
+                        borderColor: theme.darkThemeSurfaceColor_3,
+                    },
+                    "&.Mui-expanded": {
+                        borderBottomLeftRadius: 0,
+                        borderBottomRightRadius: 0,
                     },
                     "& .MuiAccordionSummary-expandIconWrapper": {
                         color: theme.themeMainTextColor,
                     },
                 }}
             >
-                <Typography
-                    variant="subtitle1"
-                    fontWeight={"bold"}
-                    sx={{ marginBottom: "6px" }}
-                >
+                <Typography variant="subtitle1" fontWeight={"bold"}>
                     {summaryText}
                 </Typography>
             </AccordionSummary>
             <AccordionDetails
                 sx={{
                     padding: 0,
+                    backgroundColor: theme.darkThemeSurfaceColor_2,
+                    borderBottomLeftRadius: "4px",
+                    borderBottomRightRadius: "4px",
+                    boxShadow: `inset 0 4px 6px -4px rgba(0, 0, 0, 0.4)`,
                 }}
             >
                 {children}
