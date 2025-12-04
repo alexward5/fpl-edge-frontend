2f04d93b6cca9cc9d1a5fd0f817e8c906f18ffea
Alex Ward <alexanderward5@gmail.com>
2025-12-03 20:07:59 -0700

    adjust accordion box shadow

--- src/components/PlayerDataDrawer/DrawerContent/DrawerAccordion/DrawerAccordion.tsx
+++ src/components/PlayerDataDrawer/DrawerContent/DrawerAccordion/DrawerAccordion.tsx
@@ -62,7 +62,7 @@ export default function DrawerAccordion({ summaryText, children }: Props) {
                     borderTop: "none",
                     borderBottomLeftRadius: BORDER_RADIUS,
                     borderBottomRightRadius: BORDER_RADIUS,
-                    boxShadow: "inset 0 4px 6px -2px rgba(0, 0, 0, 0.5)",
+                    boxShadow: "inset 0 6px 10px -4px rgba(0, 0, 0, 0.4)",
                 }}
             >
                 {children}
